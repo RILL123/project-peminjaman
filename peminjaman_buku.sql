edbfa56e97148cb75baeede39b6e7583b548422e
Syaril The Warrior <syahrilganiakbar@gmail.com>
2026-04-06 10:49:27 +0700

    Add files via upload

--- peminjaman_buku.sql
+++ peminjaman_buku.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2026 at 06:23 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 06 Apr 2026 pada 05.50
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buku`
+-- Struktur dari tabel `buku`
 --
 
 CREATE TABLE `buku` (
@@ -39,7 +39,7 @@ CREATE TABLE `buku` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `buku`
+-- Dumping data untuk tabel `buku`
 --
 
 INSERT INTO `buku` (`id_buku`, `cover`, `judul`, `kategori`, `penulis`, `tahun`, `stok`, `created_at`) VALUES
@@ -49,20 +49,20 @@ INSERT INTO `buku` (`id_buku`, `cover`, `judul`, `kategori`, `penulis`, `tahun`,
 (9, 'Adventure_Hikes.jpg', 'Adventure Hikes', 'Non-Fiksi', 'Edi Kurniawan', '2022', 49, '2026-01-29 08:02:52'),
 (10, 'The_Bike_Guy.jpg', 'The Bike Guy', 'Fiksi', 'Edi Kurniawan', '2024', 96, '2026-01-29 08:07:08'),
 (11, 'The_World_Need_More_Love.jpg', 'The World Need More Love', 'Non-Fiksi', 'Steven', '2024', 67, '2026-01-29 08:12:25'),
-(14, 'Wild_Adventure.jpg', 'Wild Adventure', 'Novel', 'Fira Feona', '2025', 77, '2026-01-29 09:37:45'),
+(14, 'Wild_Adventure.jpg', 'Wild Adventure', 'Novel', 'Fira Feona', '2025', 76, '2026-01-29 09:37:45'),
 (17, 'Balis_Day_of_Silence.jpg', 'Bali\'s Day of Silence', 'Novel', 'Fira Feona', '2025', 45, '2026-01-29 09:45:57'),
 (18, 'Choir_Festival.jpg', 'Choir Festival', 'Novel', 'Fira Feona', '2026', 88, '2026-01-29 09:52:20'),
-(19, 'Camp_V.jpg', 'Camp V', 'Novel', 'Fira Feona', '2022', 84, '2026-01-29 10:13:13'),
+(19, 'Camp_V.jpg', 'Camp V', 'Novel', 'Fira Feona', '2022', 83, '2026-01-29 10:13:13'),
 (20, 'Sebuah_Pohon.jpg', 'Sebuah Pohon', 'Novel', 'Fira Feona', '2026', 23, '2026-01-29 10:38:15'),
-(21, 'Alam.jpg', 'Alam', 'Novel', 'Fira Feona', '2026', 33, '2026-01-29 10:40:50'),
-(22, 'Celebrate_Forest.jpg', 'Celebrate Forest', 'Ensiklopedia', 'Steven', '2023', 57, '2026-01-29 10:44:01'),
-(23, 'World_Forest_Day.jpg', 'World Forest Day', 'Non-Fiksi', 'Steven', '2016', 72, '2026-01-29 10:48:47'),
-(24, 'Protect_Our_Forest.jpg', 'Protect Our Forest', 'Fiksi', 'Steven', '2021', 49, '2026-01-29 10:53:58');
+(21, 'Alam.jpg', 'Alam', 'Novel', 'Fira Feona', '2026', 32, '2026-01-29 10:40:50'),
+(22, 'Celebrate_Forest.jpg', 'Celebrate Forest', 'Ensiklopedia', 'Steven', '2023', 40, '2026-01-29 10:44:01'),
+(23, 'World_Forest_Day.jpg', 'World Forest Day', 'Non-Fiksi', 'Steven', '2016', 71, '2026-01-29 10:48:47'),
+(24, 'Protect_Our_Forest.jpg', 'Protect Our Forest', 'Fiksi', 'Steven', '2021', 48, '2026-01-29 10:53:58');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detail_peminjaman`
+-- Struktur dari tabel `detail_peminjaman`
 --
 
 CREATE TABLE `detail_peminjaman` (
@@ -73,16 +73,19 @@ CREATE TABLE `detail_peminjaman` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `detail_peminjaman`
+-- Dumping data untuk tabel `detail_peminjaman`
 --
 
 INSERT INTO `detail_peminjaman` (`id_detail`, `id_peminjaman`, `id_buku`, `jumlah`) VALUES
-(1, 1, 9, 1);
+(6, 6, 24, 0),
+(21, 21, 9, 0),
+(33, 33, 14, 0),
+(35, 35, 23, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `laporan`
+-- Struktur dari tabel `laporan`
 --
 
 CREATE TABLE `laporan` (
@@ -95,7 +98,54 @@ CREATE TABLE `laporan` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `peminjaman`
+-- Struktur dari tabel `log_aktivitas`
+--
+
+CREATE TABLE `log_aktivitas` (
+  `id_log` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `aktivitas` varchar(100) DEFAULT NULL,
+  `keterangan` text DEFAULT NULL,
+  `id_buku` int(11) DEFAULT NULL,
+  `tanggal` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `log_aktivitas`
+--
+
+INSERT INTO `log_aktivitas` (`id_log`, `id_user`, `aktivitas`, `keterangan`, `id_buku`, `tanggal`) VALUES
+(31, 1, 'Pengembalian Buku', 'Mengembalikan buku', 10, '2026-04-02 03:21:25'),
+(32, 1, 'Pengembalian Buku', 'Mengembalikan buku', 9, '2026-04-02 03:21:26'),
+(33, 1, 'Pengembalian Buku', 'Mengembalikan buku', 8, '2026-04-02 03:21:26'),
+(34, 1, 'Pengembalian Buku', 'Mengembalikan buku', 24, '2026-04-02 03:21:27'),
+(35, 1, 'Pengembalian Buku', 'Mengembalikan buku', 24, '2026-04-02 03:21:27'),
+(36, 1, 'Pengembalian Buku', 'Mengembalikan buku', 21, '2026-04-02 03:21:29'),
+(37, 1, 'Pengembalian Buku', 'Mengembalikan buku', 6, '2026-04-02 03:21:30'),
+(38, 1, 'Pengembalian Buku', 'Mengembalikan buku', 20, '2026-04-02 03:21:31'),
+(39, 1, 'Pengembalian Buku', 'Mengembalikan buku', 10, '2026-04-02 03:21:31'),
+(40, 1, 'Login User', 'User login ke sistem', NULL, '2026-04-06 03:16:06'),
+(41, 2, 'Login User', 'User login ke sistem', NULL, '2026-04-06 03:27:15'),
+(53, 1, 'Hapus Buku', 'Menghapus buku: cihuy', NULL, '2026-04-06 04:01:00'),
+(54, 2, 'Request Peminjaman', 'Selama 3 hari', 14, '2026-04-06 04:04:26'),
+(55, 2, 'Request Peminjaman', 'Selama 2 hari', 18, '2026-04-06 04:04:33'),
+(58, 2, 'Request Peminjaman', 'Selama 2 hari', 20, '2026-04-06 04:09:53'),
+(60, 2, 'Request Peminjaman', 'Selama 3 hari', 23, '2026-04-06 04:15:36'),
+(61, 1, 'Terima Request Peminjaman', 'Dari Rill untuk buku World Forest Day', 23, '2026-04-06 04:15:54'),
+(62, 2, 'Login User', 'User login ke sistem', NULL, '2026-04-06 04:39:09'),
+(63, 2, 'Request Peminjaman', 'Selama 2 hari', 23, '2026-04-06 04:40:35'),
+(64, 2, 'Request Peminjaman', 'Selama 2 hari', 23, '2026-04-06 04:40:38'),
+(65, 2, 'Request Peminjaman', 'Selama 2 hari', 20, '2026-04-06 04:43:38'),
+(66, 2, 'Request Peminjaman', 'Selama 2 hari', 20, '2026-04-06 04:44:23'),
+(67, 1, 'Login User', 'User login ke sistem', NULL, '2026-04-06 04:44:38'),
+(68, 1, 'Pengembalian Buku', 'Mengembalikan buku', 20, '2026-04-06 05:34:18'),
+(69, 1, 'Tambah Peminjaman', 'Membuat peminjaman baru', 10, '2026-04-06 05:34:38'),
+(70, 1, 'Pengembalian Buku', 'Mengembalikan buku', 10, '2026-04-06 05:34:42');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
 --
 
 CREATE TABLE `peminjaman` (
@@ -106,16 +156,20 @@ CREATE TABLE `peminjaman` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `peminjaman`
+-- Dumping data untuk tabel `peminjaman`
 --
 
 INSERT INTO `peminjaman` (`id_peminjaman`, `id_user`, `tanggal_pinjam`, `tanggal_kembali`) VALUES
-(1, 2, '2026-02-04', '2026-02-20');
+(6, 2, '2026-02-25', '2026-02-28'),
+(20, 2, '2026-04-01', '2026-04-04'),
+(21, 10, '2026-04-01', '2026-04-04'),
+(33, 2, '2026-04-06', '2026-04-09'),
+(35, 2, '2026-04-06', '2026-04-09');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengembalian`
+-- Struktur dari tabel `pengembalian`
 --
 
 CREATE TABLE `pengembalian` (
@@ -129,7 +183,32 @@ CREATE TABLE `pengembalian` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `request_peminjaman`
+--
+
+CREATE TABLE `request_peminjaman` (
+  `id_request` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_buku` int(11) DEFAULT NULL,
+  `tanggal_request` datetime DEFAULT current_timestamp(),
+  `status` enum('pending','diterima','ditolak') DEFAULT 'pending',
+  `alasan_penolakan` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `request_peminjaman`
+--
+
+INSERT INTO `request_peminjaman` (`id_request`, `id_user`, `id_buku`, `tanggal_request`, `status`, `alasan_penolakan`) VALUES
+(27, 2, 23, '2026-04-06 00:00:00', 'pending', NULL),
+(28, 2, 23, '2026-04-06 00:00:00', 'pending', NULL),
+(29, 2, 20, '2026-04-06 00:00:00', 'pending', NULL),
+(30, 2, 20, '2026-04-06 00:00:00', 'pending', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -142,25 +221,28 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id_user`, `nama`, `username`, `password`, `role`, `created_at`) VALUES
 (1, 'Admin Perpus', 'admin', 'admin123', 'admin', '2026-01-21 08:24:52'),
-(2, 'Rill', 'rill', 'rill123', 'user', '2026-01-21 08:24:52');
+(2, 'Rill', 'rill', 'rill123', 'user', '2026-01-21 08:24:52'),
+(8, 'Edi', 'Edi', '123', 'user', '2026-04-01 07:42:00'),
+(9, 'Fitri', 'Fitri', '123', 'user', '2026-04-01 07:53:35'),
+(10, 'Restu', 'Restu123', '123', 'user', '2026-04-01 08:38:38');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `buku`
+-- Indeks untuk tabel `buku`
 --
 ALTER TABLE `buku`
   ADD PRIMARY KEY (`id_buku`);
 
 --
--- Indexes for table `detail_peminjaman`
+-- Indeks untuk tabel `detail_peminjaman`
 --
 ALTER TABLE `detail_peminjaman`
   ADD PRIMARY KEY (`id_detail`),
@@ -168,94 +250,136 @@ ALTER TABLE `detail_peminjaman`
   ADD KEY `fk_detail_buku` (`id_buku`);
 
 --
--- Indexes for table `laporan`
+-- Indeks untuk tabel `laporan`
 --
 ALTER TABLE `laporan`
   ADD PRIMARY KEY (`id_laporan`);
 
 --
--- Indexes for table `peminjaman`
+-- Indeks untuk tabel `log_aktivitas`
+--
+ALTER TABLE `log_aktivitas`
+  ADD PRIMARY KEY (`id_log`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
+-- Indeks untuk tabel `peminjaman`
 --
 ALTER TABLE `peminjaman`
   ADD PRIMARY KEY (`id_peminjaman`),
   ADD KEY `fk_peminjaman_user` (`id_user`);
 
 --
--- Indexes for table `pengembalian`
+-- Indeks untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
   ADD PRIMARY KEY (`id_pengembalian`),
   ADD KEY `fk_pengembalian_peminjaman` (`id_peminjaman`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `request_peminjaman`
+--
+ALTER TABLE `request_peminjaman`
+  ADD PRIMARY KEY (`id_request`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `buku`
+-- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
--- AUTO_INCREMENT for table `detail_peminjaman`
+-- AUTO_INCREMENT untuk tabel `detail_peminjaman`
 --
 ALTER TABLE `detail_peminjaman`
-  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
--- AUTO_INCREMENT for table `laporan`
+-- AUTO_INCREMENT untuk tabel `laporan`
 --
 ALTER TABLE `laporan`
   MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `peminjaman`
+-- AUTO_INCREMENT untuk tabel `log_aktivitas`
 --
-ALTER TABLE `peminjaman`
-  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `log_aktivitas`
+  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
--- AUTO_INCREMENT for table `pengembalian`
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
   MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `request_peminjaman`
+--
+ALTER TABLE `request_peminjaman`
+  MODIFY `id_request` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `detail_peminjaman`
+-- Ketidakleluasaan untuk tabel `detail_peminjaman`
 --
 ALTER TABLE `detail_peminjaman`
   ADD CONSTRAINT `fk_detail_buku` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE,
   ADD CONSTRAINT `fk_detail_peminjaman` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE CASCADE;
 
 --
--- Constraints for table `peminjaman`
+-- Ketidakleluasaan untuk tabel `log_aktivitas`
+--
+ALTER TABLE `log_aktivitas`
+  ADD CONSTRAINT `log_aktivitas_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE,
+  ADD CONSTRAINT `log_aktivitas_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE SET NULL;
+
+--
+-- Ketidakleluasaan untuk tabel `peminjaman`
 --
 ALTER TABLE `peminjaman`
   ADD CONSTRAINT `fk_peminjaman_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE;
 
 --
--- Constraints for table `pengembalian`
+-- Ketidakleluasaan untuk tabel `pengembalian`
 --
 ALTER TABLE `pengembalian`
   ADD CONSTRAINT `fk_pengembalian_peminjaman` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `request_peminjaman`
+--
+ALTER TABLE `request_peminjaman`
+  ADD CONSTRAINT `request_peminjaman_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `request_peminjaman_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
